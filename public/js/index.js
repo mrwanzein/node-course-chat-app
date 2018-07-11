9d30ae95fba90c06263ad5a637812e6a73e393bd
Mrwan Zein <mrwanzein@outlook.com>
2018-07-11 12:24:01 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
     console.log('Connection made to server');
-
-    socket.emit('createMessage', {
-        from:'Andrew',
-        text:'Ok, I get it now'
-    });
 });
 
 socket.on('disconnect', function() {
