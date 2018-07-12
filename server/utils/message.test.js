25cfc26f25c963186b9c60a56e20fe8315a0706f
Mrwan Zein <mrwanzein@outlook.com>
2018-07-12 13:44:38 -0400

    Forgot to add generateLocationMessage test case (forgot to save before push)

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 let expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct geolocation object', () => {
+        let from = 'Bob';
+        let lat = 14;
+        let lng = 54;
+        let url = `https://www.google.com/maps?q=$14,54`;
+        let message = generateLocationMessage(from, lat, lng);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
     });
 });
